24bb43b8df66e5485218f68e52ab0f46bde87671
NataliaDuenas <104275525+NataliaDuenas@users.noreply.github.com>
2024-04-05 12:50:55 -0500

    Error en llaves

--- Main.java
+++ Main.java
@@ -7,6 +7,7 @@ import com.github.javafaker.Faker;
 import java.util.Scanner;
 //import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Random;
 //import java.io.FileReader;
 //import java.nio.file.Files;
@@ -16,7 +17,7 @@ public class Prototipo1 {
     
 
     public static void main(String[] args) throws IOException{
-            cuadros_segun_hora();
+        crear_llave();
     }
     public static void cuadros_segun_hora(){
             Faker faker=new Faker();
@@ -60,24 +61,24 @@ public class Prototipo1 {
         System.out.print("Cual es la cantidad de jugadores: ");
         int cantidad=scan.nextInt();
         long start=System.nanoTime();
-        //SinglyLinkedList lista=new SinglyLinkedList();
-        //ingresa la cantidad de jugadores
+        DinamicArray ArregloDinamico1 = new DinamicArray(2);
         for(int i=0;i<cantidad;i++){
             if(i%10000==0){
                 System.out.println(i);
                 long duration=(System.nanoTime()-start)/1000000;
                 System.out.println(duration + "ms");
             }
-            //lista.reset_puntero();
             String nombre=faker.name().fullName();
-            //lista.agregar_ordenado(nombre,random.nextInt(10000 + 1) + 0 );
+            ArregloDinamico1.agregar_ordenado(nombre,random.nextInt(10000 + 1) + 0 );
         }
-        //lista.imprimir_ranking();
+        ArregloDinamico1.printArray();
         long duration=(System.nanoTime()-start)/1000000;
         System.out.println(duration + "ms");
         
     }
-    /*public static void crear_llave(){
+   
+           
+    public static void crear_llave(){
         Random random=new Random();
         Faker faker=new Faker();
         Scanner scan=new Scanner(System.in);
@@ -87,11 +88,14 @@ public class Prototipo1 {
         //calcula el numero de rondas con el logaritmo base 2, pues se descartan de a 2 jugadores por llave
         int rondas=(int) (Math.ceil(Math.log(cantidad)/Math.log(2)));
 
-        //SinglyLinkedList lista=new SinglyLinkedList();
+        DinamicArray ArregloDinamico1 = new DinamicArray(2);
         int cont=0;
         String[] Llave1=new String[(int)Math.pow(2,rondas)];
-        for(int i=0;i<Llave1.length;i=i+2){
-            Llave1[i]= faker.name().fullName();    
+        System.out.println("Llave1 "+ Math.pow(2,rondas));
+        int n = Llave1.length;
+        for(int i=0;i<cantidad;i++){
+            int index = (2*i+1)%(n);
+            Llave1[index]= faker.name().fullName();    
             cont++;
             if(i%10000==0){
                 System.out.println(i);
@@ -99,19 +103,21 @@ public class Prototipo1 {
                 System.out.println(duration + "ms");
             }
         }
+        
+        System.out.println("Llave1 " + Arrays.toString(Llave1));
         int i=1;
         while(cont<cantidad){           
             cont=cont+2;
             Llave1[i]=faker.name().fullName();
             Llave1[Llave1.length-i]=faker.name().fullName();
             i=i+2;                
-            if (cont+1==cantidad){
-                Llave1[i]=faker.name().fullName();
-                cont++;
+            if (cantidad % 2 != 0){
+                 Llave1[Llave1.length - 1] = faker.name().fullName();
+                break;
             }
         }
         System.out.println("ronda 1");
-        for(int j=0;j<Llave1.length;j=j+2){
+        for(int j=0;j<(Llave1.length);j=j+2){
             System.out.print(Llave1[j]);
             System.out.println(" vs "+Llave1[j+1] );
         }
@@ -119,51 +125,52 @@ public class Prototipo1 {
         for (int j=0;j<(Llave1.length);j=j+2){
             if (random.nextBoolean()){
                 if(Llave1[j]==null){
-                    lista.addFirst(Llave1[j+1]);
+                    ArregloDinamico1.pushBack(Llave1[j+1]);
                 }else{
-                    lista.addFirst(Llave1[j]);
+                    ArregloDinamico1.pushBack(Llave1[j]);
                 }
             }
             else{
                 if(Llave1[j]==null){
-                    lista.addFirst(Llave1[j+1]);
+                    ArregloDinamico1.pushBack(Llave1[j+1]);
                 }else{
-                    lista.addFirst(Llave1[j]);
+                    ArregloDinamico1.pushBack(Llave1[j]);
                 }
             }
         }
         System.out.println("ronda 2");
-        lista.texto_torneo();
+        ArregloDinamico1.texto_torneo();
         cont_ronda++;
+        System.out.println("Capacidad arrwglo "+ArregloDinamico1.getCapacidad());
         while(cont_ronda<=(rondas-2)){
             System.out.println("ronda "+(cont_ronda+2));
-            
-            int tamaño_lista=lista.size();
-            for (int j=0;j<(tamaño_lista/2);j++){
+            int cont_G=0;
+             
+           for (int j = ArregloDinamico1.getSize(); j > 3; j -= 2) {
             if (random.nextBoolean()){//el verdadero representa que gano el de la izquierda
-                lista.remove_Especial(true);
+   
+                ArregloDinamico1.remove_Especial(true,j);
             }
             else{
-                lista.remove_Especial(false);
+                ArregloDinamico1.remove_Especial(false,j);
             }
+            cont_G++;
             }
             cont_ronda++;
             
-            lista.texto_torneo();
-            lista.reset_puntero();
-            
+            ArregloDinamico1.texto_torneo();           
             
         }
         System.out.println("ganador es: ");
         if(random.nextBoolean()){
-            System.out.println(lista.removeFirst());
+            ArregloDinamico1.removeBack();
         }
         else{
-            lista.removeFirst();
-            lista.texto();
+            ArregloDinamico1.removeBack();
+            ArregloDinamico1.texto();
         }
         long duration=(System.nanoTime()-start)/1000000;
         System.out.println(duration + "ms");
         
-}*/
 }
+    }
